83e14bdafdbfd1f004c2b0a55dd69245a7ced7db
Chao-Hung, Wan <qchwan@gmail.com>
2022-03-22 16:57:16 +0800

    YouBike program homework

---
+++ app.js
@@ -0,0 +1,32 @@
+// 程式碼寫這裡
+async function creatsearchresult(input) {
+  const searchresult = []
+  const API =
+    "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
+  const rawData = await fetch(API)
+  const users = await rawData.json()
+  users.filter((element) => {
+    if (element.ar.includes(input)) {
+      searchresult.push(`<li class="list-group-item fs-5">
+        <i class="fas fa-bicycle"></i>
+        ${element.sna.slice(11)}<br>
+        <small class="text-muted">${element.ar}</small>
+      </li>`)
+    }
+  })
+  return searchresult
+}
+function queryResult(input) {
+  const result = creatsearchresult(input)
+  result.then((item) =>
+    document.querySelector(".siteList").insertAdjacentHTML("afterbegin", item)
+  )
+}
+function addTodo() {
+  const input = document.querySelector("#searchKeyword")
+  queryResult(input.value)
+}
+document.querySelector(".btn").addEventListener("click", (event) => {
+  event.preventDefault()
+  addTodo()
+})
