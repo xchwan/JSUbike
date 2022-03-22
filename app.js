d2dc67d829ddc104700197bd09e6374bc5a619bd
Chao-Hung, Wan <qchwan@gmail.com>
2022-03-22 21:34:31 +0800

    fix function and variable naming

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 // 程式碼寫這裡
-async function creatsearchresult(input) {
-  const searchresult = []
+async function creatSearchResult(input) {
+  const searchResult = []
   const API =
     "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
   const rawData = await fetch(API)
   const users = await rawData.json()
   users.filter((element) => {
     if (element.ar.includes(input)) {
-      searchresult.push(`<li class="list-group-item fs-5">
+      searchResult.push(`<li class="list-group-item fs-5">
         <i class="fas fa-bicycle"></i>
         ${element.sna.slice(11)}<br>
         <small class="text-muted">${element.ar}</small>
       </li>`)
     }
   })
-  return searchresult.join(" ")
+  return searchResult.join(" ")
 }
 function queryResult(input) {
-  const result = creatsearchresult(input)
+  const result = creatSearchResult(input)
   result.then((item) =>
     document.querySelector(".siteList").insertAdjacentHTML("afterbegin", item)
   )
 }
-function addTodo() {
+function addList() {
   const input = document.querySelector("#searchKeyword")
   queryResult(input.value)
 }
-function deleteTodo() {
+function deleteList() {
   const result = document.querySelector(".siteList")
   while (result.lastChild) {
     result.removeChild(result.lastChild)
@@ -34,6 +34,6 @@ function deleteTodo() {
 }
 document.querySelector(".btn").addEventListener("click", (event) => {
   event.preventDefault()
-  deleteTodo()
-  addTodo()
+  deleteList()
+  addList()
 })
